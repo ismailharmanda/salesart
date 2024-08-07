68d90261addd5a16d1d6e045e3067b15769267a5
İsmail Harmanda <iharmanda@hotmail.com>
2024-08-08 02:00:35 +0300

    feat: integrate expo-local-authentication and conditionally render screens based on authentication status
    - Added expo-local-authentication to handle biometric authentication.
    - Implemented conditional rendering to switch between AuthScreen and ListScreen based on authentication success.
    - Enhanced error handling for failed authentication attempts.

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,8 +1,22 @@
 import {View, Button, StyleSheet} from 'react-native';
+import {
+  authenticateAsync,
+  LocalAuthenticationResult,
+} from 'expo-local-authentication';
 
-const AuthScreen = () => {
-  const handleLoginPress = () => {
-    console.log('Login Pressed');
+interface AuthScreenProps {
+  handleAuth: (result: LocalAuthenticationResult) => void;
+  onFailedAuth: (error: unknown) => void;
+}
+
+const AuthScreen = ({handleAuth, onFailedAuth}: AuthScreenProps) => {
+  const handleLoginPress = async () => {
+    try {
+      const result = await authenticateAsync();
+      handleAuth(result);
+    } catch (error) {
+      onFailedAuth(error);
+    }
   };
   return (
     <View style={styles.container}>

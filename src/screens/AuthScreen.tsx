bb63d62316b357cb8be0e1913dfe3281b3407bd1
İsmail Harmanda <iharmanda@hotmail.com>
2024-08-08 03:47:19 +0300

    feat: redirect users to 'security' page when predefined security level is not set

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,7 +1,9 @@
-import {View, Button, StyleSheet} from 'react-native';
+import {View, Button, StyleSheet, Alert, Linking, Platform} from 'react-native';
 import {
   authenticateAsync,
   LocalAuthenticationResult,
+  getEnrolledLevelAsync,
+  SecurityLevel,
 } from 'expo-local-authentication';
 
 interface AuthScreenProps {
@@ -9,9 +11,29 @@ interface AuthScreenProps {
   onFailedAuth: (error: unknown) => void;
 }
 
+const isAndroid = Platform.OS === 'android';
 const AuthScreen = ({handleAuth, onFailedAuth}: AuthScreenProps) => {
   const handleLoginPress = async () => {
     try {
+      const enrolledLevel = await getEnrolledLevelAsync();
+      if (enrolledLevel === SecurityLevel.NONE) {
+        Alert.alert(
+          'No security set up',
+          "You haven't set up any security on your device. Please set up a screen lock.",
+          [
+            {
+              text: 'Navigate to settings to set up a screen lock',
+              onPress: () =>
+                isAndroid
+                  ? Linking.sendIntent('android.settings.SECURITY_SETTINGS')
+                  : Linking.openSettings(),
+            },
+            {text: 'Cancel'},
+          ],
+        );
+        return;
+      }
+
       const result = await authenticateAsync();
       handleAuth(result);
     } catch (error) {
